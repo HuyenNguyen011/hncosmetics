64a784896f681d1c9229faf1a5e5a187376d6de6
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2021-01-14 15:23:46 +0700

    Update footer.php

--- wp-content/themes/hncosmetics/footer.php
+++ wp-content/themes/hncosmetics/footer.php
@@ -1,3 +1,21 @@
-<?php wp_footer(); ?> 
-</body> 
-</html> 
+<footer class="p-3 mt-5" id="footer" style="background-color: #ffb3a9; color: #fff">
+        <div class="footer-widgets">
+            <?php if (is_active_sidebar('sidebar-footer')): ?>
+                <div class="container">
+                    <div class="row">
+                    <?php dynamic_sidebar('sidebar-footer'); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="absolute-widgets">
+            <div class="container">
+                <div class="copyright-footer text-center">
+                    &copy H&N Cosmetics
+                </div>
+            </div>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
